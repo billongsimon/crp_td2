3418714b1d50b7fd979967feeb0b768401ac0c60
dodo <dodo@guinot.com>
2019-11-22 14:13:52 +0100

    ma page administration

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Article;
+use App\Entity\Categorie;
 
 class AdminController extends AbstractController
 {
@@ -13,7 +18,26 @@ class AdminController extends AbstractController
     public function index()
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        'controller_name' => 'AdminController',
         ]);
-    }
+     } 
+         /**
+     * @Route("/admin", name="admin")
+     */
+
+     public function listArticles(PaginatorInterface $paginator, Request $request)
+     {
+         $repo = $this->getDoctrine()->getRepository(Article::class);
+         
+         $articles = $paginator->paginate(
+             $repo->findAll(),
+             $request->query->getInt('page', 1), /*page number*/
+              18 /*limit per page*/     );
+ 
+         return $this->render('admin/index.html.twig', [
+             'controller_name' => 'AdminController',
+             'articles'=>$articles
+         ]);
+        }    
+    
 }
